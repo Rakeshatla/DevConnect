54f40631f5e1aef9489bff1039f94d01f8b4e138
Rakeshatla <151203741+Rakeshatla@users.noreply.github.com>
2025-07-01 18:50:37 +0530

    orign changed

--- src/app.js
+++ src/app.js
@@ -9,9 +9,17 @@ const profileRouter = require('./routes/profile')
 const requestRouter = require('./routes/request')
 const userRouter = require('./routes/user')
 const cors = require('cors')
-app.use(cors({
-    origin: "http://localhost:5173", credentials: true
-}))
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://devsphere-ui.onrender.com"
+        ],
+        credentials: true,
+    })
+);
+
+
 app.use('/', authRouter)
 app.use('/', profileRouter)
 app.use('/', requestRouter)
